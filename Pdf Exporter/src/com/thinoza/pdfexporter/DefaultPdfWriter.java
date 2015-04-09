3fceb9f9e08f86582fdeafe3febd0485d4c0f9b6
Damien <damienmarble@hotmail.com>
2015-04-09 16:06:31 -0700

    Added a default PdfWriter implementation.

---
+++ Pdf Exporter/src/com/thinoza/pdfexporter/DefaultPdfWriter.java
@@ -0,0 +1,34 @@
+package com.thinoza.pdfexporter;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.pdfbox.exceptions.COSVisitorException;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.interactive.form.PDField;
+
+/**
+ * A simple implementation of {@link PdfFieldWriter}.
+ * 
+ * @author Damien
+ *
+ */
+public class DefaultPdfWriter implements PdfFieldWriter {
+
+	@Override
+	public void writePdf(File originPdf, File destination,
+			Map<String, String> fieldMappings) throws IOException {
+		try (PDDocument document = PDDocument.load(originPdf)) {
+			List<PDField> pdfFields = document.getDocumentCatalog().getAcroForm().getFields();
+			for (PDField field : pdfFields) {
+				field.setValue(fieldMappings.get(field.getFullyQualifiedName()));
+			}
+			document.save(destination);
+			document.close();
+		} catch (COSVisitorException e) {
+			e.printStackTrace();
+		}
+	}
+}
