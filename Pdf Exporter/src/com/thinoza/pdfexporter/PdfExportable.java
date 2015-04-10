2d13ee22a2a726bd2038cab661bf9d92f6be05ae
Damien <damienmarble@hotmail.com>
2015-04-09 19:22:43 -0700

    Added return in docs.
    Removed unnecessary import.

--- Pdf Exporter/src/com/thinoza/pdfexporter/PdfExportable.java
+++ Pdf Exporter/src/com/thinoza/pdfexporter/PdfExportable.java
@@ -1,7 +1,8 @@
 package com.thinoza.pdfexporter;
 
 /**
- * Annotation for mapping object to pdf form fields when using the {@link ReflectionFieldExtractor}.
+ * Annotation for mapping object to pdf form fields when using the
+ * {@link ReflectionFieldExtractor}.
  * 
  * @author Damien
  *
@@ -10,7 +11,7 @@ public @interface PdfExportable {
 	/**
 	 * The name of the pdf field that this object field is mapped to.
 	 * 
-	 * @return
+	 * @return the pdf field name
 	 */
 	public String fieldName();
 }
--- Pdf Exporter/src/com/thinoza/pdfexporter/ReflectionFieldExtractor.java
+++ Pdf Exporter/src/com/thinoza/pdfexporter/ReflectionFieldExtractor.java
@@ -10,7 +10,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.stream.Collectors;
 import java.util.Set;
 
 /**
